36146d667476f62c9d1551a2a1296c68d8a7faaf
Alex Grigore <146381585+agworkgit@users.noreply.github.com>
2025-11-16 18:22:05 +0000

    mobile UI patches

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -1,5 +1,5 @@
-import "./testimonials.css";
-import { TestimonialData } from "./TestimonialData";
+import './testimonials.css';
+import { TestimonialData } from './TestimonialData';
 // import Swiper core and required modules
 import { Pagination } from 'swiper/modules';
 
@@ -11,11 +11,15 @@ import 'swiper/css/pagination';
 
 const Testimonials = () => {
     return (
-        <section className="testimonials section">
-            <h2 className="section-title">Testimonials</h2>
-            <span className="section-subtitle">What my collaborators and clients think</span>
+        <section className='testimonials section'>
+            <h2 className='section-title'>Testimonials</h2>
+            <span className='section-subtitle'>
+                What my collaborators and clients think
+            </span>
 
-            <Swiper className="testimonials-container" pagination={{ dynamicBullets: true, clickable: true }}
+            <Swiper
+                className='testimonials-container container'
+                pagination={{ dynamicBullets: true, clickable: true }}
                 modules={[Pagination]}
                 grabCursor={true}
                 slidesPerView={2}
@@ -23,30 +27,36 @@ const Testimonials = () => {
                 // loop={true}
                 breakpoints={{
                     350: {
-                        slidesPerView: 1
+                        slidesPerView: 1,
                     },
                     576: {
-                        slidesPerView: 1
+                        slidesPerView: 1,
                     },
                     768: {
                         slidesPerView: 2,
-                        spaceBetween: 48
-                    }
+                        spaceBetween: 48,
+                    },
                 }}
             >
                 {TestimonialData.map(({ id, image, title, description }) => {
                     return (
-                        <SwiperSlide className="testimonial-card" key={id}>
-                            <img src={image} alt="Image of author" className="testimonial-image" />
+                        <SwiperSlide className='testimonial-card' key={id}>
+                            <img
+                                src={image}
+                                alt='Image of author'
+                                className='testimonial-image'
+                            />
 
-                            <h3 className="testimonial-name">{title}</h3>
-                            <p className="testimonial-description">{description}</p>
+                            <h3 className='testimonial-name'>{title}</h3>
+                            <p className='testimonial-description'>
+                                {description}
+                            </p>
                         </SwiperSlide>
-                    )
+                    );
                 })}
             </Swiper>
         </section>
-    )
-}
+    );
+};
 
 export default Testimonials;
